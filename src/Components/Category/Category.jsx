9ce2f4c8c24eebf84cbd957c155690d2bdd12882
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-05-21 05:13:59 +0600

    create category card with swiper

---
+++ src/Components/Category/Category.jsx
@@ -0,0 +1,66 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination,Autoplay } from "swiper/modules";
+import categoryImg1 from "../../assets/images/home/slide1.jpg";
+import categoryImg2 from "../../assets/images/home/slide2.jpg";
+import categoryImg3 from "../../assets/images/home/slide3.jpg";
+import categoryImg4 from "../../assets/images/home/slide4.jpg";
+import categoryImg5 from "../../assets/images/home/slide5.jpg";
+
+const categoryImages = [
+  {
+    image: categoryImg1,
+    title:"SALAD"
+  },
+  {
+    image: categoryImg2,
+    title:"PIZZA"
+  },
+  {
+    image: categoryImg3,
+    title:"SOUP"
+  },
+  {
+    image: categoryImg4,
+    title:"DESSERT"
+  },
+  {
+    image: categoryImg5,
+    title:"SALAD"
+  },
+];
+
+const Category = () => {
+  return (
+    <div className="my-16">
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination,Autoplay]}
+        className="mySwiper"
+      >
+        {categoryImages.map((data, idx) => (
+          <SwiperSlide key={idx}>
+            <div className="relative">
+            <img className="rounded-xl" src={data.image} alt="Category-card-image" />
+            <div className="text-center absolute bottom-5 right-0 left-0">
+                <p className="text-xl font-medium text-white">{data.title}</p>
+            </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Category;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import Banner from "../../Components/Banner/Banner";
+import Category from "../../Components/Category/Category";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <div className="max-w-screen-xl mx-auto">
+            <Category></Category>
+            </div>
         </div>
     );
 };
