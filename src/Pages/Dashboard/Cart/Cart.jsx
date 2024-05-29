61e9a20d929468fe3f1ac591093ade7fd7ad1e1e
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-05-29 11:58:02 +0600

    implement delete action on my cart item

--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -1,16 +1,49 @@
-import { FaDeleteLeft } from "react-icons/fa6";
+import Swal from "sweetalert2";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import useCart from "../../../Hooks/useCart";
 import { MdDelete } from "react-icons/md";
+import useAxios from "../../../Hooks/useAxios";
 
 const Cart = () => {
-  const [cart] = useCart();
-  console.log(cart);
+  const [cart, refetch] = useCart();
+  const axisoSecure = useAxios();
   const totalPrice = cart.reduce(
     (accumulator, item) => accumulator + item.price,
     0
   );
-  console.log(totalPrice);
+
+  const handleCartItemDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const res = await axisoSecure.delete(`/carts/${id}`);
+          if (res.status === 200) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+            refetch();
+          }
+        } catch (error) {
+          Swal.fire({
+            title: "Error!",
+            text: "There was an error deleting your item.",
+            icon: "error",
+          });
+        }
+      }
+    });
+  };
+
   return (
     <div className="p-16">
       <div className="flex justify-center text-center">
@@ -64,9 +97,12 @@ const Cart = () => {
                     </div>
                   </td>
                   <td>{itemData.name}</td>
-                  <td>&{itemData.price}</td>
+                  <td>${itemData.price}</td>
                   <th>
-                    <button className="btn bg-red-600 btn-xs">
+                    <button
+                      onClick={() => handleCartItemDelete(itemData._id)}
+                      className="btn bg-red-600 btn-xs"
+                    >
                       <MdDelete className="text-xl text-white"></MdDelete>
                     </button>
                   </th>
