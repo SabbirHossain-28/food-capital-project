2f639d89d8de71ffecc943a044d7578a80b4ec35
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-05-30 15:46:45 +0600

    implement axios interceptor functionalities

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -1,9 +1,36 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure=axios.create({
     baseURL:"http://localhost:5000",
 })
 const useAxios = () => {
+    const {logOut}=useAuth();
+    const navigate=useNavigate();
+
+    // Request interceptor to add authorization header for every secure call to the api
+    axiosSecure.interceptors.request.use(function(config){
+        const token=localStorage.getItem("access-token");
+        // console.log("Request stoped by interceptor",token);
+        // console.log(config);
+        config.headers.authorization=`Bearer ${token}`
+        return config
+    },function(error){
+        return Promise.reject(error);
+    })
+
+    axiosSecure.interceptors.response.use(function(response){
+        return response;
+    },async function(error){
+        const status=error.response.status;
+        console.log("status error in the interceptor",error);
+        if(status===401 || status===403){
+            await logOut()
+            navigate("/login")
+        }
+        return Promise.reject(error)
+    })
    return axiosSecure
 };
 
